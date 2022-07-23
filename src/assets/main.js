77fb180761c616155652f7f6dafaac9ab3be37c1
henryshop73 <henrymoranfreelance@gmail.com>
2022-07-23 13:19:33 -0500

    Agregando ejemplo consumiendo API de youtube

---
+++ src/assets/main.js
@@ -0,0 +1,44 @@
+const API ='https://youtube-v31.p.rapidapi.com/search?channelId=UCIBObdyoPGID5tLdZ6VqVWg&part=snippet%2Cid&order=date&maxResults=5'
+
+const content = null || document.getElementById('content');
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'ee2662a126msh8d1f99bd97cd4fcp1397bcjsnc8cb97c7f05a',
+		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+	}
+};
+
+//Lógica de async: ir por los datos, luego esperar por ellos y finalmente retornarlos hacia el usuario
+async function fetchData(urlApi) { //Siempre async antes de function
+    const response = await fetch(urlApi, options); //Hacemos uso del fetch() y solo por esta vez le pasamos las opciones
+    const data = await response.json(); //Estructura de los datos transformándolos a json
+    return data; //Retorna la información de la API que estamos solicitando
+}
+
+(async () => { //Dentro implementamos la lógica necesaria para hacerel llamado a la API, obtener los elementos y mostrarlos en html
+    //Se implementa try y catch
+    try {
+        const videos = await fetchData(API);
+        let view = `
+        ${videos.items.map(video => `
+        <div class="group relative">
+            <div
+                class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+                <img src="${video.snippet.thimbnail.high.url}" alt="${video.snippet.description}" class="w-full">
+            </div>
+            <div class="mt-4 flex justify-between">
+                <h3 class="text-sm text-gray-700">
+                    <span aria-hidden="true" class="absolute inset-0"></span>
+                    ${video.snippet.title}
+                </h3>
+            </div>
+        </div>
+        `).slice(0,4).join('')}
+        
+        `;
+    }catch {
+
+    }
+})();
